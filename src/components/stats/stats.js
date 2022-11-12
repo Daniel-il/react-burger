610220c5763cf7c69c0649cfa5b9b9b039a465b5
daniel <ilyinwork05@gmail.com>
2022-11-12 19:29:18 +0300

    feat: added digits on orderboard

--- src/components/stats/stats.js
+++ src/components/stats/stats.js
@@ -1,5 +1,24 @@
-
+import statsStyles from "./stats.module.css";
 
 export function Stats() {
-    return 
+  return (
+    <article className={statsStyles.stats}>
+      <div className={statsStyles.orders}>
+        <div className={statsStyles.done}>
+          <p className="text text_type_main-medium mb-6">Готовы:</p>
+          <ul className={statsStyles.done__list}>
+            <li className={statsStyles.done__item}>
+            <p className={`${statsStyles.done__digits} text text_type_digits-default`}>1234567890</p>
+            </li>
+            <li className={statsStyles.done__item}>
+            <p className={`${statsStyles.done__digits} text text_type_digits-default`}>1234567890</p>
+            </li>
+            <li className={statsStyles.done__item}>
+            <p className={`${statsStyles.done__digits} text text_type_digits-default`}>1234567890</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </article>
+  );
 }
--- src/pages/feed.js
+++ src/pages/feed.js
@@ -1,8 +1,10 @@
 import feedStyles from "./feed.module.css";
 import { CardOrder} from "../components/card-order/card-order";
+import { Stats } from "../components/stats/stats";
 export function FeedPage() {
   return (
     <section className={feedStyles.feed}>
+      <div className={feedStyles.feed__wrapper}>
       <h1
         className={`${feedStyles.title} text text_type_main-large mt-10 mb-5`}
       >
@@ -16,6 +18,8 @@ export function FeedPage() {
         <CardOrder />
         
       </ul>
+      </div>
+      <Stats />
     </section>
   );
 }
