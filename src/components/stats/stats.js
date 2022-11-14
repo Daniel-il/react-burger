167fec30c72134d278938958fc940faa5bf897ca
Daniel-il <ilyin@DESKTOP-D9MBSO0>
2022-11-14 18:05:04 +0300

    some changes

--- src/components/stats/stats.js
+++ src/components/stats/stats.js
@@ -18,6 +18,20 @@ export function Stats() {
             </li>
           </ul>
         </div>
+        <div className={statsStyles.done}>
+          <p className="text text_type_main-medium mb-6">Готовы:</p>
+          <ul className={statsStyles.done__list}>
+            <li className={statsStyles.done__item}>
+            <p className={`${statsStyles.done__digits} text text_type_digits-default`}>1234567890</p>
+            </li>
+            <li className={statsStyles.done__item}>
+            <p className={`${statsStyles.done__digits} text text_type_digits-default`}>1234567890</p>
+            </li>
+            <li className={statsStyles.done__item}>
+            <p className={`${statsStyles.done__digits} text text_type_digits-default`}>1234567890</p>
+            </li>
+          </ul>
+        </div>
       </div>
     </article>
   );
